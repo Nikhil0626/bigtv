54a60363f63da70688e55f2f784eceee572435bb
sivakumar <jogisivakumar.eee@gmail.com>
2025-07-21 11:33:29 +0530

    ad passion sticky update

--- lib/main.dart
+++ lib/main.dart
@@ -137,7 +137,7 @@ class _MyAppState extends State<MyApp> {
             useMaterial3: true,
           ),
           routes: {
-            '/': (context) => BannerListPage(),
+            '/': (context) => SplashScreen(),
             '/settings': (context) => SettingsView(),
           },
 
