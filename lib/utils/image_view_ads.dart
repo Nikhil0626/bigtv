72e5012fb256092046f1c3bb7adc3e0f5884a4d1
sivakumar <jogisivakumar.eee@gmail.com>
2025-08-13 12:05:36 +0530

    CUBE ADS IMPLEMENTATIONS

--- lib/utils/image_view_ads.dart
+++ lib/utils/image_view_ads.dart
@@ -2,7 +2,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_carousel_slider/carousel_slider.dart';
 
-import 'app_colors.dart';
 
 class ImagePostSlider extends StatelessWidget {
   final imageUrl;
